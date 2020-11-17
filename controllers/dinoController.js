25ea6427231ee95d463894c1b0ef25bcef217d0d
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-11-17 08:15:21 -0800

    set up files and first dinos route implemented

---
+++ controllers/dinoController.js
@@ -0,0 +1,12 @@
+const { raw } = require('express')
+const fs = require('fs')
+const dinoRouter = require('express').Router()
+
+dinoRouter.get("/", (req, res) => {
+
+    const rawDinos = fs.readFileSync("./dinosaurs.json")
+    const dinos = JSON.parse(rawDinos)
+    res.render('dinosaurs/index', { dinos })
+})
+
+module.exports = dinoRouter
---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')()
+const layouts = require('express-ejs-layouts')
+
+const dinoRouter = require('./controllers/dinoController')
+
+
+app.set('view engine', 'ejs')
+app.use(layouts)
+
+
+app.get('/', (req, res) => {
+    res.send('hello i live')
+})
+
+app.use('/dinosaurs', dinoRouter)
+
+app.listen(8000, () => {
+    console.log('server started')
+})
